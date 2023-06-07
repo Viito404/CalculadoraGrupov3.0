d4497f3a01be8c473710bb324f040a6606911c33
Viitor Montunes <79266568+Viito404@users.noreply.github.com>
2023-06-07 16:11:07 -0300

    Refatorado menu.

--- Program.cs
+++ Program.cs
@@ -8,12 +8,10 @@
                string operador = "";
 
                Console.WriteLine("Entre com uma opção:");
-               Console.WriteLine("\nSoma(1)");
-               Console.WriteLine("\nDivisão(2)");
-               Console.WriteLine("\nSubtracao(3)");
-               Console.WriteLine("\nMultiplicação(4)");
+               Console.WriteLine("\nSoma (1)\nDivisão (2)\nSubtracao (3)\nMultiplicação (4)");
                Console.WriteLine("\nSair(S)");
-               opcao = Console.ReadLine();
+               Console.Write("\n> ");
+               opcao = Console.ReadLine().ToLower();
 
                switch (opcao)
                {
@@ -22,8 +20,7 @@
                     case "3": operador = "-"; break;
                     case "4": operador = "*"; break;
                     case "s": Environment.Exit(0); break;
-                    default: Environment.Exit(0); Console.WriteLine("Saindo..."); break;
-
+                    default: Console.WriteLine("Entre com uma opção válida!"); break;
                }
 
                Console.WriteLine($"\nA operação escolhida foi {operador}.");
