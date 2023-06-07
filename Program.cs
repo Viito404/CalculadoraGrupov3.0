f7816b22bcf5b2ed4db24ed0f37c5ee6506256bc
Leonardo <contato.leoo0@gmail.com>
2023-06-07 15:30:13 -0300

    implementado a função de subtração no menu

--- Program.cs
+++ Program.cs
@@ -10,13 +10,15 @@
             Console.WriteLine("Entre com uma opção:");
             Console.WriteLine("\nSoma(1)");
             Console.WriteLine("\nDivisão(2)");
+            Console.WriteLine("\nSubtracao(3)");
             Console.WriteLine("\nSair(S)");
             opcao = Console.ReadLine();
 
             switch (opcao)
             {
                 case "1": operador = "+"; break;
-                case "2": operador = "-"; break;
+                case "2": operador = "/"; break;
+                case "3": operador = "-"; break;
                 case "s": Environment.Exit(0); break;
                     default: Environment.Exit(0); Console.WriteLine("Saindo..."); break;
                 
