0713b7de2eef6c6e18515c39d892724627fbf930
Viitor Montunes <79266568+Viito404@users.noreply.github.com>
2023-06-07 15:55:29 -0300

    Indentei outra parada.

--- Program.cs
+++ Program.cs
@@ -1,28 +1,28 @@
 ﻿namespace CalculadoraGrupov3._0
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            string opcao = "";
-            string operador = "";
+     internal class Program
+     {
+          static void Main(string[] args)
+          {
+               string opcao = "";
+               string operador = "";
 
-            Console.WriteLine("Entre com uma opção:");
-            Console.WriteLine("\nSoma(1)");
-            Console.WriteLine("\nDivisão(2)");
-            Console.WriteLine("\nSubtracao(3)");
-            Console.WriteLine("\nSair(S)");
-            opcao = Console.ReadLine();
+               Console.WriteLine("Entre com uma opção:");
+               Console.WriteLine("\nSoma(1)");
+               Console.WriteLine("\nDivisão(2)");
+               Console.WriteLine("\nSubtracao(3)");
+               Console.WriteLine("\nSair(S)");
+               opcao = Console.ReadLine();
 
-            switch (opcao)
-            {
-                case "1": operador = "+"; break;
-                case "2": operador = "/"; break;
-                case "3": operador = "-"; break;
-                case "s": Environment.Exit(0); break;
+               switch (opcao)
+               {
+                    case "1": operador = "+"; break;
+                    case "2": operador = "/"; break;
+                    case "3": operador = "-"; break;
+                    case "s": Environment.Exit(0); break;
                     default: Environment.Exit(0); Console.WriteLine("Saindo..."); break;
-                
-            }
+
+               }
 
                Console.WriteLine($"\nA operação escolhida foi {operador}.");
 
@@ -33,5 +33,5 @@
                Console.Write("\nEntre com o segundo valor:\n> ");
                int valor2 = Convert.ToInt32(Console.ReadLine());
           }
-    }
+     }
 }
