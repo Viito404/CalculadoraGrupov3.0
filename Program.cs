2c36c14a486cd5f3576575d6c4bf8a70e87268f5
Viitor Montunes <79266568+Viito404@users.noreply.github.com>
2023-06-07 15:14:51 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CalculadoraGrupov3._0
+{
+     internal class Program
+     {
+          static void Main(string[] args)
+          {
+               string opcao = "";
+
+               Console.WriteLine("Entre com uma opção:");
+               Console.WriteLine("\nSoma(1)");
+               opcao = Console.ReadLine();
+          }
+     }
+}
