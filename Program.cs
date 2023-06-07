ddf8508bc7ad5f882f597c492ce51112eed233e2
jotaskalski <juan.skalski96@hotmail.com>
2023-06-07 15:43:19 -0300

    branch de multiplicação

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@
             Console.WriteLine("\nSoma(1)");
             Console.WriteLine("\nDivisão(2)");
             Console.WriteLine("\nSubtracao(3)");
+            Console.WriteLine("\nMultiplicação(4)");
             Console.WriteLine("\nSair(S)");
             opcao = Console.ReadLine();
 
@@ -19,10 +20,12 @@
                 case "1": operador = "+"; break;
                 case "2": operador = "/"; break;
                 case "3": operador = "-"; break;
+                case "4": operador = "*"; break;
                 case "s": Environment.Exit(0); break;
                     default: Environment.Exit(0); Console.WriteLine("Saindo..."); break;
                 
             }
+
 
 
         }
