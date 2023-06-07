89af3bee13769e968b3a1595acd9910af384c427
jotaskalski <juan.skalski96@hotmail.com>
2023-06-07 15:25:21 -0300

    Criando menu divisão

--- Program.cs
+++ Program.cs
@@ -1,14 +1,28 @@
 ﻿namespace CalculadoraGrupov3._0
 {
-     internal class Program
-     {
-          static void Main(string[] args)
-          {
-               string opcao = "";
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "";
+            string operador;
 
-               Console.WriteLine("Entre com uma opção:");
-               Console.WriteLine("\nSoma(1)");
-               opcao = Console.ReadLine();
-          }
-     }
+            Console.WriteLine("Entre com uma opção:");
+            Console.WriteLine("\nSoma(1)");
+            Console.WriteLine("\nDivisão(2)");
+            Console.WriteLine("\nSair(S)");
+            opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1": operador = "+"; break;
+                case "2": operador = "-"; break;
+                case "s": Environment.Exit(0); break;
+                    default: Environment.Exit(0); Console.WriteLine("Saindo..."); break;
+                
+            }
+
+
+        }
+    }
 }
